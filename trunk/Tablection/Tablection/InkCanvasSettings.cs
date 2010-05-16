600fa4ca491d840c6b02bd60aac5c6cd880817fe
dugel13@gmail.com <dugel13@gmail.com@001af8ad-599a-f9f0-d94e-71ba1c934c06>
2010-05-16 05:31:36 +0000

    첫업로드

---
+++ trunk/Tablection/Tablection/InkCanvasSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Ink;
+using System.Windows.Controls;
+
+namespace TablectionSketch
+{
+    public static class InkCanvasSettings
+    {
+        public static event Action<InkCanvasEditingMode> EditingModeChanged;
+        public static event Action<DrawingAttributes> DrawingAttributeChanged;
+
+        private static InkCanvasEditingMode _editingMode;
+        public static InkCanvasEditingMode EdtingMode
+        {
+            get { return _editingMode; }
+            set 
+            { 
+                _editingMode = value;
+
+                if (EditingModeChanged != null)
+                {
+                    EditingModeChanged(_editingMode);                    
+                }
+            }
+        }
+
+        private static DrawingAttributes _drawingAttributes;
+        public static DrawingAttributes DrawingAttributes
+        {
+            get { return _drawingAttributes; }
+            set 
+            { 
+                _drawingAttributes = value;
+
+                if (DrawingAttributeChanged != null)
+                {
+                    DrawingAttributeChanged(_drawingAttributes);
+                }
+            }
+        }
+    }
+}
