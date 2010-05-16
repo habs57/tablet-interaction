4a6558d5027f250880638260749c236ef8fa81b8
dugel13 <dugel13@gmail.com>
2010-05-16 05:31:36 +0000

    첫업로드

---
+++ Tablection/Tablection/Data/Slide.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Media;
+
+namespace TablectionSketch
+{
+    public class Slide
+    {
+        public ImageSource Image { get; set; }
+    }
+}
