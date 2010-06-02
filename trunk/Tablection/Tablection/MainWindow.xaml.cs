c90671aae06a783691710fbc827173dcc5088d71
dugel13@gmail.com <dugel13@gmail.com@001af8ad-599a-f9f0-d94e-71ba1c934c06>
2010-06-02 14:02:55 +0000

    슬라이드 리스트 애니메이션 적용

--- trunk/Tablection/Tablection/MainWindow.xaml.cs
+++ trunk/Tablection/Tablection/MainWindow.xaml.cs
@@ -21,52 +21,19 @@ namespace TablectionSketch
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        DispatcherTimer _toolShowTimer = new DispatcherTimer();
-        DispatcherTimer _slideShowTimer = new DispatcherTimer();
-
-
-        Storyboard _toolHideAnimation;
-        Storyboard _slideHideAnimation;
-        
-       
+    {  
         public MainWindow()
         {
             InitializeComponent();
-
-
-            //this._slideHideAnimation = this.FindResource("slideListHideAnimaton") as Storyboard;
-            //this._toolHideAnimation = this.FindResource("toolPanelHideAnimaton") as Storyboard;
-
-            //_slideShowTimer.Interval = TimeSpan.FromSeconds(3);
-            //_slideShowTimer.Tick += new EventHandler(_slideShowTimer_Tick);
-
-            //_toolShowTimer.Interval = TimeSpan.FromSeconds(3);
-            //_toolShowTimer.Tick += new EventHandler(_toolShowTimer_Tick);
         }
 
-        //void _slideShowTimer_Tick(object sender, EventArgs e)
-        //{
-        //    this._slideShowTimer.Stop();
-        //    this._slideHideAnimation.Begin();
-            
-        //}
-
-        //void _toolShowTimer_Tick(object sender, EventArgs e)
-        //{
-        //    this._toolHideAnimation.Stop();
-        //    this._toolHideAnimation.Begin();
-        //}
-
         private void btnBottom_Click(object sender, RoutedEventArgs e)
-        {
-            //this.RestartSlideTimer();
+        {        
             this.SlideList.Visibility = System.Windows.Visibility.Visible;
         }
 
         private void btnTop_Click(object sender, RoutedEventArgs e)
-        {
-            //this.RestartToolTimer();
+        {         
             this.ToolPanel.Visibility = Visibility.Visible;
         }
         
@@ -105,32 +72,32 @@ namespace TablectionSketch
         {
             
         }
-
-        //private void RestartSlideTimer()
-        //{
-        //    if (this._slideShowTimer.IsEnabled == false)
-        //    {
-        //        this._slideShowTimer.Start();
-        //    }
-        //}
-
-        //private void RestartToolTimer()
-        //{
-        //    if (this._toolShowTimer.IsEnabled == false)
-        //    {
-        //        this._toolShowTimer.Start();
-        //    }
-        //}
-
+        
         private void SlideList_TouchEnter(object sender, TouchEventArgs e)
         {
-            
+            System.Diagnostics.Debug.WriteLine("SlideList_TouchEnter");
         }
 
         private void SlideList_TouchLeave(object sender, TouchEventArgs e)
         {
-
+            System.Diagnostics.Debug.WriteLine("SlideList_TouchLeave");
         }
+
+        private void ToolPanel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("ToolPanel_MouseLeave");
+        }
+
+        private void SlideList_MouseEnter(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("SlideList_MouseEnter");
+        }
+
+        private void SlideList_MouseLeave(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("SlideList_MouseLeave");
+        }
+
 
     }
 }
